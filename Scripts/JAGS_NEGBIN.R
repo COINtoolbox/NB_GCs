ccd1b0b592f59981fe73021299381881984ab516
bbuelens <bbuelens@gmail.com>
2015-02-17 20:07:39 +0100

    added predictions for MBH range

--- Scripts/JAGS_NEGBIN.R
+++ Scripts/JAGS_NEGBIN.R
@@ -58,12 +58,18 @@ err_sig_e<-GCS$err_sig_e
 
 ######## NB with errors ########################################################
 
+MBHx = seq(from = 0.95 * min(GCS$MBH), 
+           to = 1.05 * max(GCS$MBH), 
+           length.out = 50)
+
 jags.data3 <- list(
   N_GC = GCS$N_GC,
   MBH = GCS$MBH,
   errN_GC = GCS$N_GC_err,
   N = nrow(GCS),
-  errMBH = upMBH
+  errMBH = upMBH,
+  MBHx = MBHx,
+  M = 50
 )
 
 model.NB <- "model{
@@ -130,9 +136,17 @@ DNew[i]<-pow(PResNew[i],2)
 }
 Fit<-sum(D[1:N])
 FitNew<-sum(DNew[1:N])
+
+for (j in 1:M){
+  etax[j]<-beta.0+beta.1*MBHx[j]
+  log(mux[j])<-max(-20,min(20,etax[j]))
+  px[j]<-size/(size+mux[j])
+  prediction.NBx[j]~dnegbin(px[j],size)
+}
 }"
 inits3 <- list(beta.0=0,beta.1=0,size=0.1)
-params3 <- c("beta.0","beta.1","size","prediction.NB","MBHtrue","Fit","FitNew")
+params3 <- c("beta.0","beta.1","size","prediction.NB","MBHtrue",
+             "Fit","FitNew","prediction.NBx")
 
 jags.neg3 <- jags.model(
   data = jags.data3, 
@@ -161,7 +175,9 @@ summary(as.mcmc.list(jagssamples.nb3$size))
 
 MBHtrue<-summary(as.mcmc.list(jagssamples.nb3$MBHtrue),quantiles=0.5)
 pred.NBerr<-summary(as.mcmc.list(jagssamples.nb3$prediction.NB),quantiles=c(0.005,0.025,0.25,0.5,0.75,0.975, 0.995))
+pred.NBerrx<-summary(as.mcmc.list(jagssamples.nb3$prediction.NBx),quantiles=c(0.005,0.025,0.25,0.5,0.75,0.975, 0.995))
 pred.NB2err<-data.frame(Type=GCS$Type,NGC=GCS$N_GC,MBHtrue=MBHtrue$quantiles,MBH=GCS$MBH,mean=pred.NBerr$statistics[,1],lwr1=pred.NBerr$quantiles[,3],lwr2=pred.NBerr$quantiles[,2],lwr3=pred.NBerr$quantiles[,1],upr1=pred.NBerr$quantiles[,5],upr2=pred.NBerr$quantiles[,6],upr3=pred.NBerr$quantiles[,7])
+pred.NB2errx<-data.frame(MBHx=MBHx,mean=pred.NBerrx$statistics[,1],lwr1=pred.NBerrx$quantiles[,3],lwr2=pred.NBerrx$quantiles[,2],lwr3=pred.NBerrx$quantiles[,1],upr1=pred.NBerrx$quantiles[,5],upr2=pred.NBerrx$quantiles[,6],upr3=pred.NBerrx$quantiles[,7])
 
 S.NB1<-ggs(codasamples.nb3 ,family=c("beta"))
 S.NB2<-ggs(codasamples.nb3,family=c("size"))
@@ -217,6 +233,28 @@ ggplot(pred.NB2err,aes(x=MBH,y=NGC))+
                                                 text = element_text(size=25))
 dev.off()
 
+CairoPDF("JAGS_NBx.pdf",height=8,width=9)
+ggplot(pred.NB2err,aes(x=MBH,y=NGC))+
+  geom_ribbon(data=pred.NB2errx,aes(x=MBHx,y=mean,ymin=lwr1, ymax=upr1), alpha=0.3, fill="gray") +
+  geom_ribbon(data=pred.NB2errx,aes(x=MBHx,y=mean,ymin=lwr2, ymax=upr2), alpha=0.2, fill="gray") +
+  geom_ribbon(data=pred.NB2errx,aes(x=MBHx,y=mean,ymin=lwr3, ymax=upr3), alpha=0.1, fill="gray") +
+  geom_point(aes(colour=Type,shape=Type),size=3.25)+
+  geom_errorbar(guide="none",aes(colour=Type,ymin=NGC-N_err,ymax=NGC+N_err),alpha=0.7)+
+  geom_errorbarh(guide="none",aes(colour=Type,xmin=MBH-GCS$lowMBH,
+                                  xmax=MBH+upMBH),alpha=0.7)+
+  geom_line(data=pred.NB2errx,aes(x=MBHx,y=mean),colour="gray25",linetype="dashed",size=1.2)+
+  scale_y_continuous(trans = 'log10',breaks=trans_breaks("log10",function(x) 10^x),
+                     labels=trans_format("log10",math_format(10^.x)))+
+  scale_colour_gdocs()+
+  scale_shape_manual(values=c(19,2,8))+
+  #  theme_economist_white(gray_bg = F, base_size = 11, base_family = "sans")+
+  theme_hc()+
+  ylab(expression(N[GC]))+
+  xlab(expression(log~M[BH]/M['\u0298']))+theme(legend.position="top",plot.title = element_text(hjust=0.5),
+                                                axis.title.y=element_text(vjust=0.75),
+                                                axis.title.x=element_text(vjust=-0.25),
+                                                text = element_text(size=25))
+dev.off()
 
 
 
