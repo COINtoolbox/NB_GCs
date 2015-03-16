ed5f473d7de69fd3ddd2c13a4c60b824a3a745b3
Rafael <rafael.2706@gmail.com>
2015-03-16 09:39:09 +0100

    ok

--- Models_each_predictor/JAGS_NEGBIN_MV.R
+++ Models_each_predictor/JAGS_NEGBIN_MV.R
@@ -58,7 +58,6 @@ err_MV_T<-GCS$err_MV_T
 ######## NB with errors ########################################################
 MV_Tx = seq(from = 1.05 * min(GCS$MV_T), 
             to = 0.95 * max(GCS$MV_T), 
-            length.out = 500)
             length.out = 50)
 
 jags.data <- list(
@@ -68,7 +67,6 @@ jags.data <- list(
   N = nrow(GCS),
   err_MV_T = err_MV_T,
   MV_Tx = MV_Tx,
-  M = 500
   M = 50
  )
 
@@ -100,9 +98,6 @@ MV_T[i]~dnorm(MV_T_true[i],1/err_MV_T[i]^2);
 
 errorN[i]~dbin(0.5,2*errN_GC[i])
 
-eta[i]<-beta.0+beta.1*MV_T_true[i]+exp(errorN[i]-errN_GC[i])
-
-log(mu[i])<-max(-20,min(20,eta[i]))# Ensures that large beta values do not cause numerical problems.
 eta[i]<-beta.0+beta.1*MV_T_true[i]
 
 #log(mu[i])<-max(-20,min(20,eta[i]))# Ensures that large beta values do not cause numerical problems.
@@ -178,10 +173,6 @@ asinh_trans <- function(){
 }
 cairo_pdf("..//Figures/M_Vx2.pdf",height=8,width=9)
 ggplot(pred.NB2err,aes(x=MV_T,y=NGC))+
-  geom_ribbon(data=pred.NB2errx,aes(x=MV_Tx,y=mean,ymin=lwr1, ymax=upr1), alpha=0.3, fill="gray") +
-  geom_ribbon(data=pred.NB2errx,aes(x=MV_Tx,y=mean,ymin=lwr2, ymax=upr2), alpha=0.2, fill="gray") +
-  geom_ribbon(data=pred.NB2errx,aes(x=MV_Tx,y=mean,ymin=lwr3, ymax=upr3), alpha=0.1, fill="gray") +
-  geom_point(aes(colour=Type,shape=Type),size=3.25,alpha=0.7)+
   geom_ribbon(data=pred.NB2errx,aes(x=MV_Tx,y=mean,ymin=lwr1, ymax=upr1), alpha=0.45, fill="gray",method = "loess") +
   geom_ribbon(data=pred.NB2errx,aes(x=MV_Tx,y=mean,ymin=lwr2, ymax=upr2), alpha=0.35, fill="gray",method = "loess") +
   geom_ribbon(data=pred.NB2errx,aes(x=MV_Tx,y=mean,ymin=lwr3, ymax=upr3), alpha=0.25, fill="gray",method = "loess") +
